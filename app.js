c3f24d45551619f81fc542a2dd915d1bbd0e8039
flamesph9 <flamesphoenix9@gmail.com>
2025-07-23 11:56:48 +0100

    DEPLOYMENT

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const connectDB = require("./db/connect")
-const app = express();
 
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
+
+const connectDB = require("./db/connect") 
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 const authenticateUser = require("./middleware/authentication");
+
+const app = express();
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+app.set('trust proxy', 1); // trust first proxy
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
+
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
